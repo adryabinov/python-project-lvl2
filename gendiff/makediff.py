1e809ea3376874a26c08f804462d8a864f0aa6df
andrejrabinov <andryabinov@gmail.com>
2021-02-12 20:28:28 +0300

    add logic

--- gendiff/makediff.py
+++ gendiff/makediff.py
@@ -1,19 +1,54 @@
-import os
-import json
-import yaml
-from pathlib import Path
+from gendiff.parser import parse_file
 
-EXTENTIONS_MAP = {
-    '.yml':lambda x:yaml.load(open(x)),
-    '.yaml':lambda x:yaml.load(open(x)),
-    '.json':lambda x:json.load(open(x)),
-}
 
 def generate_diff(path1, path2):
+    data1 = parse_file(path1)
+    data2 = parse_file(path2)
+    return make_diff(data1, data2)
 
+
+def make_diff(data1, data2):
+    diff = []
+    for key in data1.keys() - data2.keys():
+        diff.append({
+            'removed': {
+                'name': key,
+                'value': data1[key]
+            }
+        })
+
+    for key in data2.keys() - data1.keys():
+        diff.append({
+            'added': {
+                'name': key,
+                'value': data2[key]
+            }
+        })
+
+    for key in data2.keys() & data1.keys():
+        if isinstance(data1[key], dict) & isinstance(data2[key], dict):
+            diff.append({
+                'stand': {
+                    'name': key,
+                    'children': make_diff(data1[key], data2[key])
+                }
+            })
+        else:
+            if data1[key] == data2[key]:
+                diff.append({
+                    'stand': {
+                        'name': key,
+                        'value': data1[key]
+                    }
+                })
+            else:
+                diff.append({
+                    'changed': {
+                        'name': key,
+                        'value': (data1[key], data2[key])
+                    }
+                })
     return diff
 
 
-def parse_file(path):
-    ext = Path(path).suffix
-    return EXTENTIONS_MAP[ext](os.path.abspath(path))
+
