25146c91a12151102fce23155c4e199402136bd4
andrejrabinov <andryabinov@gmail.com>
2021-02-12 20:28:46 +0300

    add logic

---
+++ gendiff/parser.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+import json
+import yaml
+from pathlib import Path
+
+EXT_TO_LOADER = {
+    '.yml': lambda x: yaml.load(open(x), Loader=yaml.FullLoader),
+    '.yaml': lambda x: yaml.load(open(x), Loader=yaml.FullLoader),
+    '.json': lambda x: json.load(open(x)),
+}
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('--format', '-f', help='set format of output')
+
+    return parser.parse_args()
+
+
+def parse_file(path):
+    ext = Path(path).suffix
+    return EXT_TO_LOADER[ext](os.path.abspath(path))
