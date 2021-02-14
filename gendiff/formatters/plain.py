6a4d9f8cf4f4c49aa55227602f530d1a52b8d114
andrejrabinov <andryabinov@gmail.com>
2021-02-14 21:51:26 +0300

    linter fixes

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -4,14 +4,19 @@ TYPE_TO_STR = {
         f"\nProperty \'{parent}{x['name']}\' was removed.",
     'added':
         lambda x, parent:
-        f"\nProperty \'{parent}{x['name']}\' was added with value: {value_format(x['value'])}.",
+        f"\nProperty \'{parent}{x['name']}\'"
+        f"was added with value: {value_format(x['value'])}.",
     'updated':
         lambda x, parent:
-        f"\nProperty \'{parent}{x['name']}\' was updated with value: {value_format(x['old_value'])}"
+        f"\nProperty \'{parent}{x['name']}\'"
+        f"was updated with value: {value_format(x['old_value'])}"
         f" to value: {value_format(x['new_value'])}.",
     'stand':
         lambda x, parent:
-        format_diff(x['children'], f"{parent}{x['name']}.") if ('children' in x) else ''
+        format_diff(
+            x['children'],
+            f"{parent}{x['name']}."
+        ) if ('children' in x) else ''
 }
 
 
