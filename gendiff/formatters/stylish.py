341e83638534c18eb38fa811fe609c25c43374b9
andrejrabinov <andryabinov@gmail.com>
2021-02-25 15:09:34 +0300

    refact f-n indent

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -14,47 +14,6 @@ supported_types = {
 }
 
 
-def format_tree(tree):
-    def walk(in_tree, depth=1):
-        out = []
-        for item in in_tree:
-            if item['type'] not in supported_types:
-                raise ValueError('diff or formatter is broken')
-            if item['type'] == 'removed':
-                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
-                out += f"{format_value(item['value'], depth)}"
-            if item['type'] == 'added':
-                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
-                out += f"{format_value(item['value'], depth)}"
-            if item['type'] == 'updated':
-                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
-                out += f"{format_value(item['old_value'], depth)}"
-                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
-                out += f"{format_value(item['new_value'], depth)}"
-            if item['type'] == 'nested':
-                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
-                out += f"{{{walk(item['children'], depth + 1)}}}"
-            if item['type'] == 'unchanged':
-                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
-                out += f"{format_value(item['value'], depth)}"
-        return f"{''.join(out)}\n{make_indent(depth-1)}"
-    return f"{{{walk(tree).rstrip(' ')}}}"
-
-
-def format_value(value, depth):
-    if isinstance(value, dict):
-        out = []
-        for key in value:
-            out += f"\n{make_indent(depth + 1, STAND)}{key}: "
-            out += f"{format_value(value[key], depth + 1)}"
-        return f"{{{''.join(out)}\n{make_indent(depth)}}}"
-    if isinstance(value, bool):
-        return str(value).lower()
-    if value is None:
-        return 'null'
-    return str(value)
-
-
 def make_indent(depth,
                 fix=None,
                 indent_size=INDENT_SIZE,
@@ -63,3 +22,44 @@ def make_indent(depth,
     dynamic_part = (depth - 1) * indent_size * indent_type
     fix_part = fix if fix else fix_size * indent_type
     return f"{dynamic_part}{fix_part}"
+
+
+def format_tree(tree):
+    def walk(in_tree, depth=1):
+        out = []
+        for item in in_tree:
+            if item['type'] not in supported_types:
+                raise ValueError('diff or formatter is broken')
+            if item['type'] == 'removed':
+                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
+                out += f"{stringify(item['value'], depth)}"
+            if item['type'] == 'added':
+                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
+                out += f"{stringify(item['value'], depth)}"
+            if item['type'] == 'updated':
+                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
+                out += f"{stringify(item['old_value'], depth)}"
+                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
+                out += f"{stringify(item['new_value'], depth)}"
+            if item['type'] == 'nested':
+                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
+                out += f"{{{walk(item['children'], depth + 1)}}}"
+            if item['type'] == 'unchanged':
+                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
+                out += f"{stringify(item['value'], depth)}"
+        return f"{''.join(out)}\n{make_indent(depth-1)}"
+    return f"{{{walk(tree).rstrip(' ')}}}"
+
+
+def stringify(value, depth):
+    if isinstance(value, dict):
+        out = []
+        for key in value:
+            out += f"\n{make_indent(depth + 1, STAND)}{key}: "
+            out += f"{stringify(value[key], depth + 1)}"
+        return f"{{{''.join(out)}\n{make_indent(depth)}}}"
+    if isinstance(value, bool):
+        return str(value).lower()
+    if value is None:
+        return 'null'
+    return str(value)
