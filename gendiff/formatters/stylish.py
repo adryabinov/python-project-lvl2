a9a9dbe4fa6db41d306f2ed3c4514553919bc682
andrejrabinov <andryabinov@gmail.com>
2021-02-25 13:04:35 +0300

    refact 3

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -20,21 +20,21 @@ def format_tree(tree):
             if item['type'] not in supported_types:
                 raise ValueError('diff or formatter is broken')
             if item['type'] == 'removed':
-                out += f"\n{make_indent(depth - 1)}{REMOVED}{item['name']}: "
+                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
                 out += f"{format_value(item['value'], depth)}"
             if item['type'] == 'added':
-                out += f"\n{make_indent(depth - 1)}{ADDED}{item['name']}: "
+                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
                 out += f"{format_value(item['value'], depth)}"
             if item['type'] == 'updated':
-                out += f"\n{make_indent(depth - 1)}{REMOVED}{item['name']}: "
+                out += f"\n{make_indent(depth, REMOVED)}{item['name']}: "
                 out += f"{format_value(item['old_value'], depth)}"
-                out += f"\n{make_indent(depth - 1)}{ADDED}{item['name']}: "
+                out += f"\n{make_indent(depth, ADDED)}{item['name']}: "
                 out += f"{format_value(item['new_value'], depth)}"
             if item['type'] == 'nested':
-                out += f"\n{make_indent(depth - 1)}{STAND}{item['name']}: "
+                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
                 out += f"{walk(item['children'], depth + 1)}"
             if item['type'] == 'unchanged':
-                out += f"\n{make_indent(depth - 1)}{STAND}{item['name']}: "
+                out += f"\n{make_indent(depth, STAND)}{item['name']}: "
                 out += f"{format_value(item['value'], depth)}"
         return f"{{{''.join(out)}\n{make_indent(depth - 1)}}}"
     return walk(tree)
@@ -44,7 +44,7 @@ def format_value(value, depth):
     if isinstance(value, dict):
         out = []
         for key in value:
-            out += f"\n{make_indent(depth)}{STAND}{key}: "
+            out += f"\n{make_indent(depth + 1, STAND)}{key}: "
             out += f"{format_value(value[key], depth + 1)}"
         return f"{{{''.join(out)}\n{make_indent(depth)}}}"
     if isinstance(value, bool):
@@ -54,5 +54,7 @@ def format_value(value, depth):
     return str(value)
 
 
-def make_indent(depth):
+def make_indent(depth, difficult=None):
+    if difficult:
+        return ((depth - 1) * INDENT_SIZE * INDENT_TYPE) + difficult
     return depth * INDENT_SIZE * INDENT_TYPE
